3e1df24761a3b1757c51bb86d872fdc9237e7c5c
kevin <kevin345618@outlook.com>
2020-06-05 15:32:46 +0800

    remove add bootn and input can be auto-focus now

--- src/Component/Insert.js
+++ src/Component/Insert.js
@@ -1,30 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {add_items} from '../Redux/actions'
 
 export default function Insert() {
+
     const dispatch = useDispatch()
     const [insert, setInsert] = useState("")
+
+    useEffect(() => {
+        const f = document.querySelector('.insert-place')
+        f.focus()
+    })
 
     return (
         <li className="insert-area">
             <input name="A_A"
+            className="insert-place"
             placeholder="add something here"
             type="text" 
             value={insert} 
             onChange={
                 e => setInsert(e.target.value)
             }
-            />
-            <button onClick={
-                () => {
-                    if (insert !== "") {
-                        dispatch(add_items(insert))
-                        setInsert("")
-                    }
+            onKeyDown={ event => {
+                if (event.key === 'Enter' && insert !== ''){
+                    dispatch(add_items(insert))
                 }
-            }
-            >add</button>
+            }}
+            />
         </li>
     )
 }
