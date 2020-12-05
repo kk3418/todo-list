a4851c713cf999721139d1460432fbee2d66aab5
kevin <kevin345618@outlook.com>
2020-12-06 01:11:37 +0800

    讓輸入使用起來更自然

--- src/Component/Insert.js
+++ src/Component/Insert.js
@@ -1,21 +1,21 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {useDispatch} from 'react-redux'
 import {dataSlice} from '../Redux'
 
 export default function Insert() {
-
+    const Ref = useRef()
     const dispatch = useDispatch()
     const [insert, setInsert] = useState("")
     const { add_item } = dataSlice.actions
 
     useEffect(() => {
-        const f = document.querySelector('.insert-place')
-        f.focus()
+        Ref.current.focus()
     })
 
     return (
         <li className="insert-area">
             <input name="A_A"
+            ref={Ref}
             className="insert-place"
             placeholder="add something here"
             type="text" 
@@ -23,9 +23,10 @@ export default function Insert() {
             onChange={
                 e => setInsert(e.target.value)
             }
-            onKeyDown={ event => {
+            onKeyPress={ event => {
                 if (event.key === 'Enter' && insert !== ''){
                     dispatch(add_item(insert))
+                    setInsert('')
                 }
             }}
             />
