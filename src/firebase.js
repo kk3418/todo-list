d83bbdcf7675a97f20d544f5ce43d40c4b0683f1
kevin <kevin345618@outlook.com>
2021-01-29 03:22:12 +0800

    firebaseConfig is exposed

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-}
+  apiKey: "AIzaSyDqmhdtInSvzOcCbE7tuTmWShT581ccEDE",
+  authDomain: "todo-list-f8673.firebaseapp.com",
+  projectId: "todo-list-f8673",
+  storageBucket: "todo-list-f8673.appspot.com",
+  messagingSenderId: "241758813853",
+  appId: "1:241758813853:web:be55f67296fe2a3381de74"
+};
 const app = firebase.initializeApp(firebaseConfig)
 
 export const signIn = (email, password) => app.auth().signInWithEmailAndPassword(email, password)
